6090ebb5513bd0e29a8a684361f09e1286bd3706
JadeShopKeepr <91377287+JadeShopKeepr@users.noreply.github.com>
2022-04-03 11:29:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const listItem = document.querySelectorAll('.list__item') 
+$(function activeLink (){
+   listItem.forEach((listItem) => 
+   item.classList.remove('active'));
+   this.classList.add('active');
+
+
+   
+
+})
+listItem.forEach((item) => 
+item.addEventListener('click', activeLink));
